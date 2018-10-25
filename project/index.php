57fe75402e794a15b5de73edc1e5149b8ae298d8
Mathurin Petit <mpetit@24eme.fr>
2018-10-25 19:12:27 +0200

    ajout d'un activite.csv.example et la reprise partiel des Commit / mail/ calendar helper d'affichage des date / commit ...

--- project/index.php
+++ project/index.php
@@ -44,12 +44,53 @@ if (($handle = fopen($pathFactures, "r")) !== false) {
 			}
 			$datas = array_values($datas);
 			if(!array_key_exists($typeTemps,$barsTemps)){ $barsTemps[$typeTemps] = 0.0; }
-			if(strtoupper($datas[6]) == "FAUX"){
-				$s = str_replace(array(" ",',','€'), array('','.',''), preg_replace("/\s+/", '',$datas[5]));
+			if(strtoupper($datas[4]) == "FAUX"){
+				$s = str_replace(array(" ",',','€'), array('','.',''), preg_replace("/\s+/", '',$datas[3]));
 				$restantFactures +=  floatval($s);
 			}
 		}
 	fclose($handle);
+}
+
+// calcul des types d'activités
+$pathActivites = $folderName.'/activites.csv';
+$first = true;
+$activites = array("commits" => 0,"mails" => 0,"reunions" => 0);
+if (($handle = fopen($pathActivites, "r")) !== false) {
+		while (($datas = fgetcsv($handle, 1000, ";")) !== false) {
+			if($first){
+				$first = false;
+				 continue;
+			}
+			$datas = array_values($datas);
+			$attributs = explode(',',$datas[1]);
+			$type = str_replace("Type:",'',$attributs[0]);
+			if($type == 'Commit'){
+				$activites["commits"] +=1;
+			}
+			if($type == 'Mail'){
+				$activites["mails"] +=1;
+			}
+		}
+	fclose($handle);
+}
+
+function transform($tableCase){
+	if(preg_match('/^Type:/',$tableCase)){
+		$attributs = explode(',',$tableCase);
+		if($attributs[0] && $attributs[2]){
+			$type = str_replace("Type:",'',$attributs[0]);
+			$author = str_replace("Author:",'',$attributs[2]);
+			if($type && $author){
+				return $type."&nbsp;".$author;
+			}
+		}
+	}
+	$matches = array();
+	if(preg_match('/^([0-9]{4}-[0-9]{2}-[0-9]{2})\ [0-9]{2}:[0-9]{2}:[0-9]{2}/', $tableCase, $matches)){
+		return $matches[1];
+	}
+	return str_replace('\n', "<br/>",nl2br($tableCase));
 }
 
 ?>
@@ -72,18 +113,30 @@ if (($handle = fopen($pathFactures, "r")) !== false) {
 
 		<div class="row my-4">
 			<div class="col-7">
-				<div class="row">
-				<?php foreach ($barsTemps as $barName => $barTemps) : ?>
-					<div class="col-9" >
-						<div class="progress" style="height: 30px; margin-bottom:10px;">
-						  <div class="progress-bar bg-warning text-dark" role="progressbar" aria-valuenow="<?php echo $barTemps ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ($barTemps /$barSize) * 100 ?>%"><?php echo $barName ?></div>
+				<div class="card card-default h-100">
+					<div class="card-body">
+						<div class="row">
+						<?php foreach ($barsTemps as $barName => $barTemps) : ?>
+							<div class="col-9" >
+								<div class="progress" style="height: 30px; margin-bottom:10px;">
+								  <div class="progress-bar bg-warning text-dark" role="progressbar" aria-valuenow="<?php echo $barTemps ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo ($barTemps /$barSize) * 100 ?>%"><?php echo $barName ?></div>
+								</div>
+							</div>
+							<div class="col-3"><?php echo round($barTemps * 2 ) / 2; ?> jrs</div>
+						<?php endforeach ?>
 						</div>
 					</div>
-					<div class="col-3"><?php echo round($barTemps * 2 ) / 2; ?> jrs</div>
-				<?php endforeach ?>
 				</div>
 			</div>
-			<div class="col-5 text-right">&nbsp;<h4><?php echo sprintf("%.2f",$restantFactures)." €"?><br/>restant à payer</h4></div>
+			<div class="col-5" >
+				<div class="card card-default h-100">
+					<div class="card-body">
+						<div class="text-center"><h4><?php echo sprintf("%.2f",$restantFactures)." €"?><br/>restant à payer</h4><hr/>
+							<h4><?php echo $activites["commits"]; ?>&nbsp;commits&nbsp;/&nbsp;<?php echo $activites["mails"]; ?>&nbsp;mails<br/><?php echo $activites["reunions"]; ?>&nbsp;réunions</h4>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 
 
@@ -121,13 +174,13 @@ if (($handle = fopen($pathFactures, "r")) !== false) {
 									$nb = count($datas);
 									echo '<tr>';
 									for ($i=0; $i < $nb; $i++):
-										echo ($first)?  '<th class="text-center">'.$datas[$i].'</th>' : '<td>'.$datas[$i].'</td>';
+										echo ($first)?  '<th class="text-center">'.$datas[$i].'</th>' : '<td>'.transform($datas[$i]).'</td>';
 									endfor;
 									echo '</tr>';
 									$first = false;
 								endwhile;
 								fclose($handle);
-							endif;
+							endif;$activites["commits"]
 							?>
 							</table>
 							<div class="col-12 text-center">
